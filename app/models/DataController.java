b8d78fd5e54923383c77896d920f9abdf73c55ed
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-02-28 14:36:34 +0000

    - Added classes to store data controllers and old/new formats
    - Code to store the json that we get into these classes
    - Added code in dataController view file to handle the object sent to it
    - Fixed issue in which address was not being reset at the start of every record in RegistryHandler

---
+++ app/models/DataController.java
@@ -0,0 +1,161 @@
+package models;
+
+import java.util.ArrayList;
+
+public class DataController {
+	private String registrationNumber, organisationName, companiesHouseNumber,
+			address, postcode, country, foiFlag, startDate, endDate,
+			exemptFlag, tradingName, ukContact, subjectAccess, format;
+
+	// new format
+	private NewFormat newFormat;
+	// old format
+	private ArrayList<OldFormat> oldFormat;
+
+	public DataController() {
+		registrationNumber = "";
+		organisationName = "";
+		companiesHouseNumber = "(not given)";
+		address = "";
+		postcode = "(not given)";
+		country = "(not given)";
+		foiFlag = "";
+		startDate = "";
+		endDate = "";
+		exemptFlag = "";
+		tradingName = "(not given)";
+		ukContact = "";
+		subjectAccess = "";
+		format = "";
+		oldFormat = new ArrayList<OldFormat>();
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+	public String getOrganisationName() {
+		return organisationName;
+	}
+
+	public void setOrganisationName(String organisationName) {
+		this.organisationName = organisationName;
+	}
+
+	public String getCompaniesHouseNumber() {
+		return companiesHouseNumber;
+	}
+
+	public void setCompaniesHouseNumber(String companiesHouseNumber) {
+		this.companiesHouseNumber = companiesHouseNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getFoiFlag() {
+		return foiFlag;
+	}
+
+	public void setFoiFlag(String foiFlag) {
+		this.foiFlag = foiFlag;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getExemptFlag() {
+		return exemptFlag;
+	}
+
+	public void setExemptFlag(String exemptFlag) {
+		this.exemptFlag = exemptFlag;
+	}
+
+	public String getTradingName() {
+		return tradingName;
+	}
+
+	public void setTradingName(String tradingName) {
+		this.tradingName = tradingName;
+	}
+
+	public String getUkContact() {
+		return ukContact;
+	}
+
+	public void setUkContact(String ukContact) {
+		this.ukContact = ukContact;
+	}
+
+	public String getSubjectAccess() {
+		return subjectAccess;
+	}
+
+	public void setSubjectAccess(String subjectAccess) {
+		this.subjectAccess = subjectAccess;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public NewFormat getNewFormat() {
+		return newFormat;
+	}
+
+	public void setNewFormat(NewFormat newFormat) {
+		this.newFormat = newFormat;
+	}
+
+	public ArrayList<OldFormat> getOldFormat() {
+		return oldFormat;
+	}
+
+	public void setOldFormat(ArrayList<OldFormat> oldFormat) {
+		this.oldFormat = oldFormat;
+	}
+
+}

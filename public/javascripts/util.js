c6d3293ef91549db3db61cb76908c49f0f093965
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-04-12 18:24:23 +0100

    similar controllers

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -106,6 +106,8 @@ var makeItemChart = function(type,id){
 	var number = parseInt($("#"+type+"-"+id).text());
 	var item=$("#"+id).text();
 	var diff = count - number;
+	var link = $("#" + type + "-link-"+id).attr("href");
+	console.log(link);
 	var label1,label2;
 	label1="Controllers which collect";
 	label2="Controllers which do not";
@@ -113,20 +115,20 @@ var makeItemChart = function(type,id){
 	switch(type){
 	case "dataClass":
 		graph="left-graph";	
-		var link=$("#lala").attr('href');
-		 $("#left-heading").empty().append("<h4>Popularity of Data Class ("+item+")</h4><p>This shows how many controllers collect this data class and how many do not</p>");
+		//var link=$("#lala").attr('href');
+		 $("#left-heading").empty().append("<h4>Popularity of Data Class ("+item+")</h4><p>This shows how many controllers collect this data class and how many do not</p><a href=\" "+link+"\">View controllers collecting this data class</a>");
 		 break;
 	case "sensitiveData":
 		graph="left-graph";
-		$("#left-heading").empty().append("<h4>Popularity of Sensitive Data Class ("+item+")</h4><p>This shows how many controllers collect this data class and how many do not</p>");
+		$("#left-heading").empty().append("<h4>Popularity of Sensitive Data Class ("+item+")</h4><p>This shows how many controllers collect this data class and how many do not</p><a href=\" "+link+"\">View controllers collecting this sensitive data class</a>");
 		break;
 	case "dataSubject":
 		graph="middle-graph";
-		$("#middle-heading").empty().append("<h4>Popularity of Sensitive Data Subject ("+item+")</h4><p>This shows how many controllers collect this information from this data subject and how many do not</p>");
+		$("#middle-heading").empty().append("<h4>Popularity of Sensitive Data Subject ("+item+")</h4><p>This shows how many controllers collect this information from this data subject and how many do not</p><a href=\" "+link+"\">View controllers collecting from this data subject</a>");
 		break;
 	case "dataDisclosee":
 		graph="right-graph";
-		$("#right-heading").empty().append("<h4>Popularity of Data Disclosee ("+item+")</h4><p>This shows how many controllers share information with this data disclosee and how many do not</p>");
+		$("#right-heading").empty().append("<h4>Popularity of Data Disclosee ("+item+")</h4><p>This shows how many controllers share information with this data disclosee and how many do not</p><a href=\" "+link+"\">View controllers collecting sharing information with this disclosee</a>");
 		label1="Controllers which share";
 		label2="Controllers which do not";
 		break;	 
